62104d860f6096ecf8037ad9ea318684ad125bc2
WarriorsSami <sbarbutdica@gmail.com>
2021-09-26 12:39:28 +0300

    feat(diesel): finish db connection setup
    I've created diesel migrations, psql schema for books table, proper models and support methods for CRUD operations onto the rust_api DB.

---
+++ migrations/2021-09-26-084046_create_books/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    author VARCHAR NOT NULL,
+    published BOOLEAN NOT NULL DEFAULT 'f'
+)
---
+++ src/models.rs
@@ -0,0 +1,76 @@
+use diesel;
+use diesel::prelude::*;
+use diesel::pg::{PgConnection, Pg};
+
+use crate::schema::books;
+use crate::schema::books::dsl::books as all_books;
+
+#[derive(Queryable)]
+pub struct Book {
+    pub id: i32,
+    pub title: String,
+    pub author: String,
+    pub published: bool,
+}
+
+#[derive(Insertable)]
+#[table_name = "books"]
+pub struct NewBook {
+    pub title: String,
+    pub author: String,
+    pub published: bool,
+}
+
+impl Book {
+    pub fn show(id: i32, conn: &PgConnection) -> Vec<Book> {
+        all_books
+            .find(id)
+            .load::<Book>(conn)
+            .expect("Error loading specified books")
+    }
+
+    pub fn all(conn: &PgConnection) -> Vec<Book> {
+        all_books
+            .order(books::id.desc())
+            .load::<Book>(conn)
+            .expect("Error loading all books")
+    }
+
+    pub fn update_by_id(id: i32, conn: &PgConnection, book: NewBook) -> bool {
+        use crate::schema::books::dsl::{title as t, author as a, published as p};
+        let NewBook {
+            title,
+            author,
+            published,
+        } = book;
+
+        diesel::update(all_books.find(id))
+            .set((t.eq(title), a.eq(author), p.eq(published)))
+            .get_result::<Book>(conn)
+            .is_ok()
+    }
+
+    pub fn insert(book: NewBook, conn: &PgConnection) -> bool {
+        diesel::insert_into(books::table)
+            .values(&book)
+            .execute(conn)
+            .is_ok()
+    }
+
+    pub fn delete_by_id(id: i32, conn: &PgConnection) -> bool {
+        if Book::show(id, conn).is_empty() {
+            return false;
+        }
+
+        diesel::delete(all_books.find(id))
+            .execute(conn)
+            .is_ok()
+    }
+
+    pub fn all_by_author(author: String, conn: &PgConnection) -> Vec<Book> {
+        all_books
+            .filter(books::author.eq(author))
+            .load::<Book>(conn)
+            .expect("Error loading books for specified author")
+    }
+}
