806351e721a2b16cd2ef6bb02587802f4ae4848c
WarriorsSami <sbarbutdica@gmail.com>
2021-09-26 19:58:15 +0300

    feat(r2d2): setup db pool
    I've implemented a db pool using r2d2 package to manage and decouple the existence of db connections from the requests flow. Also, I've created a connection guard struct for PgConnections via implementing the Deref and FromRequest traits.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
+extern crate r2d2;
+extern crate r2d2_diesel;
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
 
 use dotenv::dotenv;
 use std::env;
@@ -9,15 +20,25 @@ use diesel::pg::PgConnection;
 
 mod schema;
 mod models;
+mod db;
+
+//#[get("/<name>/<age>")]
+//fn hello(name: String, age: u8) -> String {
+//    format!("Hello, {} year old named {}!", age, name)
+//}
 
 fn main() {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
+    //rocket::ignite()
+    //    .mount("/hello", routes![hello])
+    //    .launch();
+
+    /*let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
     let conn = PgConnection::establish(&database_url).unwrap();
 
     let book = models::NewBook {
-        title: String::from("Ultima speranta"),
+        title: String::from("Miez de noapte"),
         author: String::from("Erin Hunter"),
         published: true,
     };
@@ -26,5 +47,5 @@ fn main() {
         println!("success");
     } else {
         println!("failed");
-    }
+    }*/
 }
